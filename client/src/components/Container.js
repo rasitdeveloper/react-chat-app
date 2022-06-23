1a2c46633f987efa72655740ec606a98d474ba33
rasitdeveloper <frasitkilic@gmail.com>
2022-06-23 06:09:26 +0300

    Fix date time

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -7,12 +7,15 @@ import Form from "./Form";
 function Container() {
   const { setChat } = useChat();
 
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " " + today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
   useEffect(() => {
     init();
 
     subscribeToMessages((message) => {
       console.log("callback function");
-      setChat((prev) => [...prev, { text: message }]);
+      setChat((prev) => [...prev, { text: message, date: time }]);
     });
   }, [setChat]);
 
